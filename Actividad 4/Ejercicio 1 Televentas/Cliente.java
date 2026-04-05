8636a32609cd01b7ae2998c77cd11519822cd7e8
Rossanapena <cpena86@unisalle.edu.co>
2026-04-05 14:14:50 -0500

    Update and rename Televentas.java to Cliente.java

---
+++ Actividad 4/Ejercicio 1 Televentas/Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente {
+
+    private String nombre;
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
