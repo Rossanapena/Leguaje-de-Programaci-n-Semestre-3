03b514afa308a1dfb7b1a4af24c70d67a082d489
Rossanapena <cpena86@unisalle.edu.co>
2026-05-09 23:14:16 -0500

    Update Building.java

--- Actividad 5/Ejercicio CarbonFootprint/Building.java
+++ Actividad 5/Ejercicio CarbonFootprint/Building.java
@@ -1,1 +1,29 @@
+public class Building implements CarbonFootprint {
 
+    private String name;
+    private double electricityConsumption;
+
+    public Building(String name, double electricityConsumption) {
+        this.name = name;
+        this.electricityConsumption = electricityConsumption;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getElectricityConsumption() {
+        return electricityConsumption;
+    }
+
+    @Override
+    public double getCarbonFootprint() {
+        return electricityConsumption * 0.5;
+    }
+
+    @Override
+    public String toString() {
+        return "Building: " + name +
+               " | Carbon Footprint: " + getCarbonFootprint();
+    }
+}
