21a9ff1a6399948c9c30a81f390cd538532a2224
Rossanapena <cpena86@unisalle.edu.co>
2026-05-09 23:34:22 -0500

    Update CarbonFootprintTest.java

--- Actividad 5/Ejercicio CarbonFootprint/CarbonFootprintTest.java
+++ Actividad 5/Ejercicio CarbonFootprint/CarbonFootprintTest.java
@@ -1,1 +1,28 @@
+public class CarbonFootprintTest {
 
+    public static void main(String[] args) {
+
+        Building building = new Building("Test Building", 100);
+
+        Car car = new Car("Test Car", 50);
+
+        Bicycle bicycle = new Bicycle("Test Bicycle", 20);
+
+        System.out.println("=== UNIT TESTS ===");
+
+        System.out.println(
+            "Building Test: " +
+            (building.getCarbonFootprint() == 50.0 ? "PASSED" : "FAILED")
+        );
+
+        System.out.println(
+            "Car Test: " +
+            (car.getCarbonFootprint() == 115.0 ? "PASSED" : "FAILED")
+        );
+
+        System.out.println(
+            "Bicycle Test: " +
+            (bicycle.getCarbonFootprint() == 1.0 ? "PASSED" : "FAILED")
+        );
+    }
+}
